2129aadd4b66baa17addf64bc5a49a730bc22d23
samplefish <mr_hoppy654@yahoo.com>
2019-09-15 04:18:35 -0700

    try swap app config?

--- src/main/java/com/samplefish/config/ServletInitializer.java
+++ src/main/java/com/samplefish/config/ServletInitializer.java
@@ -6,13 +6,12 @@ public class ServletInitializer extends AbstractAnnotationConfigDispatcherServle
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		// TODO Auto-generated method stub
-		return null;
+		return new Class[] {AppConfig.class};
 	}
 
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
-		return new Class[] {AppConfig.class};
+		return null;
 	}
 
 	@Override
